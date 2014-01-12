afc9d0f428a56334537c4f12468ec4d3f89df449
Alex Popelard <apopelard2014@kellogg.northwestern.edu>
2014-01-12 01:58:56 -0600

    addition of mailer specs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,22 @@
 Kelloggbook::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { :host => 'http://kelloggbook.herokuapp.com/' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com"
+    :port                 => 587
+    :domain               => "gmail.com"
+    :user_name            => "meetingapp.dev@gmail.com"
+    :password             => "KIEI462buddy"
+    :authentication       => "plain"
+    :enable_starttls_auto => true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
