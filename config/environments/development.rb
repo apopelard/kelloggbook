7078329a17d3cf770274e92f06475e8c97d97074
Alex Popelard <apopelard2014@kellogg.northwestern.edu>
2014-04-10 23:13:30 -0500

    remove exchange student signup selection box

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,24 @@
 Kelloggbook::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+   # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => "meetingapp.dev@gmail.com",
+    :password             => "KIEI462buddy",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
